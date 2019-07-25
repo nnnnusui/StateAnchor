46b3b11f03f1b88bafbc79d296d3cd04a1e0e213
nnnnusui <nnnnusui@gmail.com>
2019-07-26 00:05:49 +0900

    📝➕ [def getStateFromAnchor, Rich things]

--- src/main/scala/com/github/nnnnusui/stateanchor/AnchorNBTController.scala
+++ src/main/scala/com/github/nnnnusui/stateanchor/AnchorNBTController.scala
@@ -1,7 +1,13 @@
 package com.github.nnnnusui.stateanchor
 
+import com.github.nnnnusui.stateanchor.StateAnchor.{NBTKey => Key}
+import com.github.nnnnusui.stateanchor.rich.RichItem.enrich
+import com.github.nnnnusui.stateanchor.rich.RichToNBT.Enrich.blockState
+import net.minecraft.block.BlockState
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.{CompoundNBT, INBT}
+
+import scala.collection.JavaConverters._
 
 /*
  * Control NBT
@@ -9,35 +15,53 @@ import net.minecraft.nbt.{CompoundNBT, INBT}
  * in ItemStack.
  */
 object AnchorNBTController{
-  import com.github.nnnnusui.stateanchor.rich.RichItem.enrich
-  import com.github.nnnnusui.stateanchor.rich.RichToNBT.Enrich.blockState
 
   def putAnchorNBT(itemStack: ItemStack): Boolean ={
-    val defaultState = itemStack.getItem.getBlock.getDefaultState
-    val properties   = defaultState.getProperties
+    val stateContainer = itemStack.getItem.getBlock.getStateContainer
+    val properties   = stateContainer.getProperties
     if (properties.isEmpty) return false
 
     val tag
       = if(itemStack.hasTag) itemStack.getTag
         else                 new CompoundNBT
-    val stateNBT      = defaultState.toCompoundNBT
+    val stateNBT      = stateContainer.getBaseState.toCompoundNBT
     val firstProperty = properties.iterator().next()
-    putDefaultToTag(tag, stateNBT, firstProperty.getName)
+    putDefaultNBTToTag(tag, stateNBT, firstProperty.getName)
     itemStack.setTag(tag)
     true
   }
-
-  import com.github.nnnnusui.stateanchor.StateAnchor.{NBTKey => Key}
-
-  private def putDefaultToTag(nbt: CompoundNBT, state: INBT, selectedPropertyName: String): CompoundNBT ={
-    val defaultContents = generateDefault(state, selectedPropertyName)
+  private def putDefaultNBTToTag(nbt: CompoundNBT, state: INBT, selectedPropertyName: String): CompoundNBT ={
+    val defaultContents = generateDefaultNBT(state, selectedPropertyName)
     nbt.put(Key.root, defaultContents)
     nbt
   }
-  private def generateDefault(state: INBT, selectedPropertyName: String): CompoundNBT ={
+  private def generateDefaultNBT(state: INBT, selectedPropertyName: String): CompoundNBT ={
     val stateAnchor = new CompoundNBT
     stateAnchor.putString(Key.selectedProperty, selectedPropertyName)
-    stateAnchor.put(Key.state,state)
+    stateAnchor.put(Key.state, state)
     stateAnchor
+  }
+
+  def getStateFromAnchor(anchor: CompoundNBT, stateList: java.util.Collection[BlockState]): Option[BlockState]
+      = getStateFromAnchor(anchor, stateList.asScala.toSeq)
+  def getStateFromAnchor(state: CompoundNBT, stateList: Seq[BlockState]): Option[BlockState] ={
+    val index = stateList.map(it =>it.toCompoundNBT)
+                         .indexOf(state)
+    if (index < 0) return None
+    Some.apply(stateList(index))
+  }
+
+  object Rich{
+    implicit class RichItemStack(val itemStack: ItemStack){
+      def hasAnchorNBT: Boolean
+          = (itemStack.hasTag
+          && itemStack.getTag.contains(StateAnchor.NBTKey.root))
+
+      def getAnchorNBT: Option[CompoundNBT]
+          = hasAnchorNBT match {
+              case true  => Some(itemStack.getTag.getCompound(StateAnchor.NBTKey.root))
+              case false => None
+            }
+    }
   }
 }
