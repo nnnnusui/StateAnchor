6b23c84fe1cfd01d47a51ba48f28dcad7ece00dd
nnnnusui <nnnnusui@gmail.com>
2019-07-25 18:32:22 +0900

    ➕📝 Some Constants [NBTKey, KeyBind]

--- src/main/scala/com/github/nnnnusui/stateanchor/StateAnchor.scala
+++ src/main/scala/com/github/nnnnusui/stateanchor/StateAnchor.scala
@@ -6,6 +6,19 @@ import net.minecraftforge.fml.common.Mod
 class  StateAnchor
 object StateAnchor{
   final val modId = "stateanchor"
+  object KeyBind{
+    private final val header = "key.categories."
+    final val category = s"$header$modId"
+    object Description{
+      private final val header = "key."
+      final val rotateMode = s"$header$modId.rotateMode"
+    }
+  }
+  object NBTKey{
+    final val root = s"$modId"
+      final val state = "State"
+      final val selectedProperty = "SelectedProperty"
+  }
 }
 
 
